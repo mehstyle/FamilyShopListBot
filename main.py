21445d8c6e857e40bdea7748f28b73d7b3d9fe19
george <eropkatim@gmail.com>
2021-02-03 22:58:27 +0300

    Добавил подключение sqlite

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import telebot
+import sqlite3
+
+try:
+    connect = sqlite3.connect('shoppinglists.bd')
+    bd_cursor_obj = connect.cursor()
+except FileNotFoundError:
+    connect.close()
 
 
-bot = telebot.TeleBot("", parse_mode=None)
+try:
+    file = open("token.txt", encoding='utf-8')
+    token = file.read().strip()
+finally:
+    file.close()
 
+bot = telebot.TeleBot(token, parse_mode=None)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    if message.chat.type == "private":
+        bot.send_message(message.chat.id, "Привет, друг! Я телеграм-бот, который помогает сохранять список покупок!")
+    if message.chat.type == "group":
+        bot.send_message(message.chat.id, "Приветствую вас, друзья! Я телеграм-бот, который помогает сохранять список покупок!")
+
+
+bot.polling()
